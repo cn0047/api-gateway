1875df39fe0a34b845d5f279ac76b501e489c52c
Vladimir Kovpak <cn007b@gmail.com>
2018-03-24 01:29:54 +0200

    Simplified everything possible.

---
+++ src/app/service/gateway/externalAPI/ExternalAPI.go
@@ -0,0 +1,43 @@
+package externalAPI
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const (
+	timeout = 10000 // milliseconds
+)
+
+// TakeData - performs request into external JSON REST API.
+// This method performs request,
+// than unmarshal JSON and assigns payload to provided pointer.
+// It's possible to use this method for any external JSON REST API.
+func TakeData(URI string, p interface{}) error {
+	client := http.Client{Timeout: time.Millisecond * timeout}
+
+	resp, err := client.Get(URI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, p)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- src/app/service/gateway/github/GitHub.go
+++ src/app/service/gateway/github/GitHub.go
@@ -1,123 +1,52 @@
 package github
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
-	"net/http"
 	"sync"
-	"time"
 
 	"app/service/gateway/github/resource"
-	"app/service/gateway/github/response"
+	"app/service/gateway/externalAPI"
 )
 
 const (
-	gitHubAPIURL                = "https://api.github.com/users/"
-	gitHubAPITimeoutMillisecond = 10000
+	gitHubAPI = "https://api.github.com/users/"
 )
 
 // GetUserInfo - Get from github information about user, user's repos and organizations.
-func GetUserInfo(userName string) (response.User, []string) {
-	var user response.User
-	var errorsList []string
+func GetUserInfo(userName string) (map[string]interface{}, map[string]interface{}) {
+	// Errors map.
+	e := make(map[string]interface{})
+
+	// Result payload map. Contains structures which will be populated by data from github.
+	payload := make(map[string]interface{})
+	payload["profile"] = &resource.Profile{}
+	payload["repos"] = &[]resource.Repo{}
+	payload["orgs"] = &[]resource.Org{}
+
+	// Map: payload key to end-point suffix.
+	m := make(map[string]string)
+	m["profile"] = ""
+	m["repos"] = "/repos"
+	m["orgs"] = "/orgs"
+
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(len(payload))
 
-	go func() {
-		defer wg.Done()
-		data, err := getProfileInfo(userName)
-		if err == nil {
-			user.Profile = data
-		} else {
-			errorsList = append(errorsList, err.Error())
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		data, err := getReposInfo(userName)
-		if err == nil {
-			user.Repos = data
-		} else {
-			errorsList = append(errorsList, err.Error())
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		data, err := getOrgsInfo(userName)
-		if err == nil {
-			user.Orgs = data
-		} else {
-			errorsList = append(errorsList, err.Error())
-		}
-	}()
+	for k, suffix := range m {
+		key := k
+		endPoint := gitHubAPI + userName + suffix
+		data := payload[key]
+		go func() {
+			defer wg.Done()
+			err := externalAPI.TakeData(endPoint, &data)
+			if err == nil {
+				payload[key] = data
+			} else {
+				e[key] = err.Error()
+			}
+		}()
+	}
 
 	wg.Wait()
 
-	return user, errorsList
-}
-
-// Perform request into github API.
-// This method only performs request and returns response body which intended to contain payload,
-// hence it's possible to use this method for any github API end-point.
-func getDataFromGitHub(endPoint string) ([]byte, error) {
-	client := http.Client{Timeout: time.Millisecond * gitHubAPITimeoutMillisecond}
-
-	resp, err := client.Get(gitHubAPIURL + endPoint)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == 404 {
-		return nil, errors.New("resource not found")
-	}
-	if resp.StatusCode != 200 {
-		return nil, errors.New(resp.Status)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
-}
-
-// Get info about user (general profile information)
-func getProfileInfo(userName string) (resource.Profile, error) {
-	var profile resource.Profile
-	rawRepos, err := getDataFromGitHub(userName)
-	if err != nil {
-		return profile, err
-	}
-	json.Unmarshal(rawRepos, &profile)
-
-	return profile, nil
-}
-
-// Get info about user's repositories.
-func getReposInfo(userName string) ([]resource.Repo, error) {
-	var repos []resource.Repo
-	rawRepos, err := getDataFromGitHub(userName + "/repos")
-	if err != nil {
-		return repos, err
-	}
-	json.Unmarshal(rawRepos, &repos)
-
-	return repos, nil
-}
-
-// Get info about user's organizations.
-func getOrgsInfo(userName string) ([]resource.Org, error) {
-	var orgs []resource.Org
-	rawOrgs, err := getDataFromGitHub(userName + "/orgs")
-	if err != nil {
-		return orgs, err
-	}
-	json.Unmarshal(rawOrgs, &orgs)
-
-	return orgs, nil
+	return payload, e
 }
